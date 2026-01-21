c41be2232e6dbfc0e20fa19e3ab48c8da46e42fc
Charles <enagecharles27@gmail.com>
2026-01-21 23:03:38 +0800

    Optimized Header and style

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,28 +1,35 @@
 import { Link, useNavigate } from "react-router-dom"
 import { useState } from "react";
 import '../Styles/HeaderStyle.css'
+import Nav from "./Nav";
 const Header = () => {
     const [search, setSearch] = useState("");
   return (
     <header className="header">
-      <div className="header-title">
-        <h2>E-commerce Store</h2>
-        <form className="search-bar-form">
-        <input 
-          id="search-bar"
-          type="text" 
-          placeholder="Search..." 
-          className="search-bar" 
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-        ></input>
-        <button type="submit"><i className="fa-solid fa-magnifying-glass" style={{fontSize: "20px"}}></i></button>
-        </form>
-      </div>
-      <div className="shopping-cart">
-        <Link to={'/Login'} className="Login">Log-in/Sign-Up</Link>
-        <i className="fa-solid fa-cart-shopping " style={{fontSize: "25px"}}></i>
-      </div>
+      <section className="header-section">
+        <div className="header-title">
+          <h2>E-commerce Store</h2>
+          <form className="search-bar-form">
+          <input 
+            id="search-bar"
+            type="text" 
+            placeholder="Search..." 
+            className="search-bar" 
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          ></input>
+          <button type="submit"><i className="fa-solid fa-magnifying-glass" style={{fontSize: "20px"}}></i></button>
+          </form>
+        </div>
+        <div className="shopping-cart">
+          <Link to={'/Login'} className="Login">Log-in/Sign-Up</Link>
+          <button className="cart-wrapper">
+            <i className="fa-solid fa-cart-shopping" style={{fontSize: "25px"}}></i>
+            <span className="count">3</span>
+          </button>
+        </div>
+      </section>
+      <Nav />
     </header>
   )
 }
