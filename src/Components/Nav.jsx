2e0611dceddee712ba0ef9d5affd0fdb1c7720e9
Charles <enagecharles27@gmail.com>
2026-01-21 23:06:43 +0800

    add Nav-bars but as of now no link

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,9 +1,21 @@
-
-import React from 'react'
+import "../Styles/Nav.css"
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav className="nav-bar">
+      <a href="/" className = "customer-service nav-bar-ref">Home</a>
+      <div className="nav-dropdown">
+        <a href="#" className="products nav-bar-ref">Products ▾</a>
+
+        <div className="nav-dropdown-menu">
+          <a href="/phones">Phones</a>
+          <a href="/laptops">Laptops</a>
+         <a href="/accessories">Accessories</a>
+        </div>
+      </div>
+      <a href="#" className = "customer-service nav-bar-ref">Customer Service</a>
+      <a href="#" className = "contactUs nav-bar-ref">Contact Us</a>
+    </nav>
   )
 }
 
