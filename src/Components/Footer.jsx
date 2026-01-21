b6acec2f7f406f09964d14fd27cc23a690bc6e1d
Charles <enagecharles27@gmail.com>
2026-01-22 00:56:20 +0800

    footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import '../Styles/FooterStyle.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <section className="footer">
+        <h3 className="Information">Information</h3>
+        <address>
+          <div>
+          E-commerce Store<br/>
+          Manila bulletin board
+          <br/>
+          Email: <a href="mailto:inquiries@acmerockets.com">Inquires@E-commerceStore.com</a>
+          <br />
+          Phone: <a href="tel:+15555555555"> (555) 555-5555</a>
+          </div>
+          <div className="copyrights">
+            <p >Copyright &copy; <span id="year">{new Date().getFullYear()}</span></p>
+            <p >All Rights Reserved</p>
+          </div>
+        </address>
+      </section>
+    </footer>
   )
 }
 
