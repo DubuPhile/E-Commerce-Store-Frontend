851709a042ba66c2270b49faf8ffe8c0ba252535
Charles <enagecharles27@gmail.com>
2026-01-23 00:42:45 +0800

    Context ProductProvider

---
+++ src/Context/productsProvider.jsx
@@ -0,0 +1,19 @@
+import { createContext, useState } from "react"
+import data from "../data/products.json" 
+
+const initState = data.products
+console.log(initState)
+
+export const ProductsContext = createContext({ products: [] })
+
+export const ProductsProvider = ({ children }) => {
+    const [products, setProducts] = useState(initState)
+
+    return (
+        <ProductsContext.Provider value={{ products }}>
+            {children}
+        </ProductsContext.Provider>
+    )
+}
+
+export default ProductsProvider
