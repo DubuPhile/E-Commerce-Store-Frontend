c7324970ab1e15e1c1c059cc16059657735ae172
Charles <enagecharles27@gmail.com>
2026-01-24 02:09:11 +0800

    Optimize productlist

--- src/Components/productList.jsx
+++ src/Components/productList.jsx
@@ -1,31 +1,29 @@
-import useProducts from "../hooks/useProducts"
-import Products from "./products"
+import useProducts from "../hooks/useProducts";
+import Products from "./products";
+import useCart from "../hooks/useCart";
 
 const ProductsList = () => {
-    const {products} = useProducts()
+  const { dispatch, REDUCER_ACTIONS, cart } = useCart();
+  const { products } = useProducts();
 
+  let pageContent = <p>Loading...</p>;
 
-    let pageContent = <p>Loading...</p>
+  if (products?.length) {
+    pageContent = products.map((product) => {
+      const inCart = cart.some((item) => item.sku === product.sku);
+      return (
+        <Products
+          key={product.sku}
+          products={product}
+          dispatch={dispatch}
+          REDUCER_ACTIONS={REDUCER_ACTIONS}
+          inCart={inCart}
+        />
+      );
+    });
+  }
 
-    if(products?.length){
-        pageContent = products.map(product => {
-        
-        return(
-            <Products
-                key = {product.sku}
-                products={product}
-            
-            />
-        )
-        })
-    }
+  return <main className="main main--products">{pageContent}</main>;
+};
 
-
-    return (
-        <main className="main main--products">
-        {pageContent}
-        </main>
-    )
-}
-
-export default ProductsList
+export default ProductsList;
