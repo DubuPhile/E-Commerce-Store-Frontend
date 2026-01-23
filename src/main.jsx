e09237b9fe016ff9d410976cebe156f90a6c3211
Charles <enagecharles27@gmail.com>
2026-01-24 02:06:55 +0800

    add CartProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
-import ProductsProvider from './Context/productsProvider.jsx'
+import ProductsProvider from "./Context/productsProvider.jsx";
+import { CartProvider } from "./Context/cartProvider.jsx";
 
-if (process.env.NODE_ENV === 'production'){
+if (process.env.NODE_ENV === "production") {
   disableReactDevTools();
 }
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>   
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <BrowserRouter>
       <ProductsProvider>
-        <Routes>        
-          <Route path={"/*"} element={<App />} />
-        </Routes>
+        <CartProvider>
+          <Routes>
+            <Route path={"/*"} element={<App />} />
+          </Routes>
+        </CartProvider>
       </ProductsProvider>
-    </BrowserRouter>  
+    </BrowserRouter>
   </StrictMode>,
-)
+);
