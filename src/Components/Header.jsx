8cd6be7513473b2081919aee3023838170bb8d6c
Charles <enagecharles27@gmail.com>
2026-01-24 02:08:29 +0800

    navigate button and adjust the count

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,37 +1,50 @@
-import { Link, useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
-import '../Styles/HeaderStyle.css'
+import "../Styles/HeaderStyle.css";
 import Nav from "./Nav";
+import useCart from "../hooks/useCart";
 const Header = () => {
-    const [search, setSearch] = useState("");
+  const [search, setSearch] = useState("");
+  const { totalItems } = useCart();
+  const navigate = useNavigate();
   return (
     <header className="header">
       <section className="header-section">
         <div className="header-title">
           <h2>E-commerce Store</h2>
           <form className="search-bar-form">
-          <input 
-            id="search-bar"
-            type="text" 
-            placeholder="Search..." 
-            className="search-bar" 
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-          ></input>
-          <button type="submit"><i className="fa-solid fa-magnifying-glass" style={{fontSize: "20px"}}></i></button>
+            <input
+              id="search-bar"
+              type="text"
+              placeholder="Search..."
+              className="search-bar"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            ></input>
+            <button type="submit">
+              <i
+                className="fa-solid fa-magnifying-glass"
+                style={{ fontSize: "20px" }}
+              ></i>
+            </button>
           </form>
         </div>
         <div className="shopping-cart">
-          <Link to={'/Login'} className="Login">Log-in /Sign-Up</Link>
-          <button className="cart-wrapper">
-            <i className="fa-solid fa-cart-shopping" style={{fontSize: "25px"}}></i>
-            <span className="count">3</span>
+          <Link to={"/Login"} className="Login">
+            Log-in /Sign-Up
+          </Link>
+          <button className="cart-wrapper" onClick={() => navigate("/cart")}>
+            <i
+              className="fa-solid fa-cart-shopping"
+              style={{ fontSize: "25px" }}
+            ></i>
+            <span className="count">{totalItems}</span>
           </button>
         </div>
       </section>
       <Nav />
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
