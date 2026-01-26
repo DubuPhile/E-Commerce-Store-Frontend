87110edfb9bce8b8dda261f985b0946e32a702f3
Charles <enagecharles27@gmail.com>
2026-01-26 19:52:29 +0800

    Login and Logout redux

---
+++ src/features/auth/authSlice.jsx
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+  name: "auth",
+  reducers: {
+    setCredentials: (state, action) => {
+      const { username, accessToken } = action.payload;
+      state.user = username;
+      state.token = accessToken;
+    },
+    logOut: (state, action) => {
+      state.user = null;
+      state.token = null;
+    },
+  },
+});
+
+export const { setCredentials, logOut } = authSlice.actions;
+export default authSlice.reducer;
+export const selectCurrentUser = (state) => state.auth.user;
+export const selectCurrentToken = (state) => state.auth.token;
