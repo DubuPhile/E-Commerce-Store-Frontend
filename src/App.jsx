eab6e95b23e9c07e9891a6cf9d74b5d0a4e27218
Charles <enagecharles27@gmail.com>
2026-01-26 18:16:53 +0800

    LoginPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import HomePage from "./Pages/HomePage";
 import { Routes, Route, Navigate } from "react-router-dom";
 import CartPage from "./Pages/CartPage";
+import LoginPage from "./Pages/LoginPage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/cart" element={<CartPage />} />
+        <Route path="/Login" element={<LoginPage />} />
       </Routes>
     </>
   );
