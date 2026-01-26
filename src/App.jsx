88fc67fc1b58846e878ecf8fb0080a5c232b6e09
Charles <enagecharles27@gmail.com>
2026-01-27 00:30:06 +0800

    Protected Routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import HomePage from "./Pages/HomePage";
 import { Routes, Route, Navigate } from "react-router-dom";
 import CartPage from "./Pages/CartPage";
 import LoginPage from "./Pages/LoginPage";
+import RequireAuth from "./features/auth/RequireAuth";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/Login" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route element={<RequireAuth />}>
+          <Route path="/cart" element={<CartPage />} />
+        </Route>
       </Routes>
     </>
   );
