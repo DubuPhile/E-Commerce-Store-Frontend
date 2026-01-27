3de8636294628fd9758aecd3dae004350729e438
Charles <enagecharles27@gmail.com>
2026-01-28 00:48:10 +0800

    Persist Login

---
+++ src/Components/PersistLogin.jsx
@@ -0,0 +1,30 @@
+import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
+import useRefreshToken from "../hooks/useRefreshToken";
+import useAuth from "../hooks/useAuth";
+import Spinner from "./Loading";
+
+const PersistLogin = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const refresh = useRefreshToken();
+  const { auth } = useAuth();
+
+  useEffect(() => {
+    let isMounted = true;
+    const verifyRefreshToken = async () => {
+      try {
+        await refresh();
+      } catch (err) {
+        console.log(err);
+      } finally {
+        isMounted && setIsLoading(false);
+      }
+    };
+    !auth?.token ? verifyRefreshToken() : setIsLoading(false);
+    return () => (isMounted = false);
+  }, [auth?.token, refresh]);
+
+  return isLoading ? <Spinner /> : <Outlet />;
+};
+
+export default PersistLogin;
