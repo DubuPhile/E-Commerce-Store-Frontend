85ef6b0408da028f518efc64be01ddccd5c06b08
Charles <enagecharles27@gmail.com>
2026-01-28 00:51:12 +0800

    implement the Persist and roles

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import { useState } from "react";
 import HomePage from "./Pages/HomePage";
 import { Routes, Route, Navigate } from "react-router-dom";
 import CartPage from "./Pages/CartPage";
 import LoginPage from "./Pages/LoginPage";
 import RequireAuth from "./features/auth/RequireAuth";
+import PersistLogin from "./Components/PersistLogin";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route element={<RequireAuth />}>
-          <Route path="/cart" element={<CartPage />} />
+
+        <Route element={<PersistLogin />}>
+          <Route path="/" element={<HomePage />} />
+          <Route element={<RequireAuth allowedRoles={[2001, 5150]} />}>
+            <Route path="/cart" element={<CartPage />} />
+          </Route>
         </Route>
       </Routes>
     </>
