aa45f9fefc5c8d7ad373883539ff795226a1388e
Charles <enagecharles27@gmail.com>
2026-01-28 00:49:35 +0800

    implement the roles

--- src/features/auth/RequireAuth.jsx
+++ src/features/auth/RequireAuth.jsx
@@ -1,16 +1,22 @@
 import { useLocation, Navigate, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { selectCurrentToken } from "./authSlice";
+import { selectCurrentToken, selectCurrentRoles } from "./authSlice";
 
-const RequireAuth = () => {
+const RequireAuth = ({ allowedRoles }) => {
   const token = useSelector(selectCurrentToken);
+  const roles = useSelector(selectCurrentRoles);
   const location = useLocation();
 
-  return token ? (
-    <Outlet />
-  ) : (
-    <Navigate to="/login" state={{ from: location }} replace />
-  );
+  if (!token) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  } else if (
+    allowedRoles &&
+    !roles.some((role) => allowedRoles.includes(role))
+  ) {
+    return <Navigate to="/unauthorized" state={{ from: location }} replace />;
+  } else {
+    return <Outlet />;
+  }
 };
 
 export default RequireAuth;
