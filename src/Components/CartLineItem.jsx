76fd04fdb600e9454cfb5058f82dd8bff0408b6f
Charles <enagecharles27@gmail.com>
2026-02-03 00:18:06 +0800

    implement remove and change quantity

--- src/Components/CartLineItem.jsx
+++ src/Components/CartLineItem.jsx
@@ -1,7 +1,14 @@
+import {
+  useDeleteCartItemMutation,
+  useChangeQuantityMutation,
+} from "../features/cart/cartApiSlice";
+
 const CartLineItem = ({ item }) => {
+  const [deleteCartItem] = useDeleteCartItemMutation();
+  const [updateCartQty] = useChangeQuantityMutation();
   const img = item.product?.imageUrl;
-  const lineTotal = item.quantity * item.product.price;
-  const highestQty = 20 > item.quantity ? 20 : item.quantity;
+  const lineTotal = item.quantity * item.product?.price;
+  const highestQty = Math.max(20, item.quantity);
   const optionValue = [...Array(highestQty).keys()].map((i) => i + 1);
   const option = optionValue.map((val) => {
     return (
@@ -11,9 +18,25 @@ const CartLineItem = ({ item }) => {
     );
   });
 
-  const onChangeQty = (e) => {};
+  const onChangeQty = async (value) => {
+    try {
+      await updateCartQty({
+        itemId: value.itemId,
+        quantity: value.quantity,
+      }).unwrap();
+    } catch (err) {
+      console.error("Failed to update quantity");
+    }
+  };
 
-  const onRemoveFromCart = () => {};
+  const onRemoveFromCart = async (productId) => {
+    try {
+      await deleteCartItem(productId).unwrap();
+      console.log("item deleted Successfully");
+    } catch (err) {
+      console.error("failed to delete item", err);
+    }
+  };
 
   return (
     <li className="cart-item">
@@ -34,7 +57,12 @@ const CartLineItem = ({ item }) => {
         className="cart-select"
         value={item.quantity}
         aria-label="Item Quantity"
-        onChange={onChangeQty}
+        onChange={(e) =>
+          onChangeQty({
+            itemId: item._id,
+            quantity: Number(e.target.value),
+          })
+        }
       >
         {option}
       </select>
@@ -48,7 +76,7 @@ const CartLineItem = ({ item }) => {
         className="cart-button"
         aria-label="Remove Item From Cart"
         title=" Remove Item From Cart"
-        onClick={onRemoveFromCart}
+        onClick={() => onRemoveFromCart(item._id)}
       >
         <i className="fa-solid fa-trash-can"></i>
       </button>
