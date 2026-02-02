325b0aeca6102099fe97458899d4a94707fe6941
Charles <enagecharles27@gmail.com>
2026-02-02 20:42:39 +0800

    implement incomplete cart page

--- src/Pages/CartPage.jsx
+++ src/Pages/CartPage.jsx
@@ -1,40 +1,63 @@
 import { useState, useEffect } from "react";
 import Layout from "../Components/Layout";
-import useCart from "../hooks/useCart";
 import CartLineItem from "../Components/CartLineItem";
+import { useGetMyCartQuery } from "../features/cart/cartApiSlice";
 
 const CartPage = () => {
   const [confirm, setConfirm] = useState(false);
-  const { dispatch, REDUCER_ACTIONS, totalItems, totalPrice, cart } = useCart();
+  const [cart, setCart] = useState([]);
+  const { data: MyCart, error, isLoading } = useGetMyCartQuery();
+
+  let pageContent = "";
+
+  useEffect(() => {
+    if (MyCart?.items) {
+      setCart(MyCart?.items);
+    }
+  }, [MyCart]);
+
+  if (isLoading) return (pageContent = <p>Loading...</p>);
+
+  const totalItems = cart.reduce((total, item) => {
+    return total + item.quantity;
+  }, 0);
+
+  const totalPrice = cart.reduce((total, item) => {
+    return total + item.quantity * item.product?.price;
+  }, 0);
+
+  const sortedItems = [...cart].sort((a, b) =>
+    a.product.name.localeCompare(b.product.name),
+  );
+
   const onSubmitOrder = () => {
-    dispatch({ type: REDUCER_ACTIONS.SUBMIT });
     setConfirm(true);
   };
-  useEffect(() => {
-    if (totalItems === 0) setConfirm(false);
-  }, [totalItems]);
 
-  const pageContent = confirm ? (
+  pageContent = confirm ? (
     <h2>Thank you for your order.</h2>
   ) : (
     <>
-      <h2>Cart</h2>
+      <h2 className="offscreen">Cart</h2>
       <ul>
         {cart.map((item) => {
-          return (
-            <CartLineItem
-              key={item.sku}
-              item={item}
-              dispatch={dispatch}
-              REDUCER_ACTIONS={REDUCER_ACTIONS}
-            />
-          );
+          return <CartLineItem key={item._id} item={item} />;
         })}
       </ul>
-      <div>
+      <div className="cart-totals">
         <p>Total Items: {totalItems}</p>
-        <p>Total Price: {totalPrice}</p>
-        <button onClick={onSubmitOrder} className="cart-submit">
+        <p>
+          Total Price:{" "}
+          {new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+          }).format(totalPrice)}
+        </p>
+        <button
+          className="cart-submit"
+          disabled={!totalItems}
+          onClick={onSubmitOrder}
+        >
           Place Order
         </button>
       </div>
