64bd698dedfbfe11abcff337bfe51468b288019c
Charles <enagecharles27@gmail.com>
2026-02-04 00:31:19 +0800

    add Product, searchProduct, getProductbyId

--- src/features/products/productsApiSlice.jsx
+++ src/features/products/productsApiSlice.jsx
@@ -6,7 +6,27 @@ export const productApiSlice = apiSlice.injectEndpoints({
       query: () => "/product",
       providesTags: ["Product"],
     }),
+    addProduct: builder.mutation({
+      query: (formData) => ({
+        url: "/product/add",
+        method: "POST",
+        body: formData,
+      }),
+      invalidatesTags: ["Product"],
+    }),
+    searchProduct: builder.query({
+      query: (searchTerm) => `/product/search?query=${searchTerm}`,
+    }),
+    getProductById: builder.query({
+      query: (Id) => `/product/get-product/${Id}`,
+      providesTags: (result, error, Id) => [{ type: "Product", Id }],
+    }),
   }),
 });
 
-export const { useGetProductsQuery } = productApiSlice;
+export const {
+  useGetProductsQuery,
+  useAddProductMutation,
+  useSearchProductQuery,
+  useGetProductByIdQuery,
+} = productApiSlice;
