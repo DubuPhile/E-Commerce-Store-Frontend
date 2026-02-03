d6fc40e61cda107eb2ae791f05463a8af99c81ae
Charles <enagecharles27@gmail.com>
2026-02-04 00:30:37 +0800

    remove add product

--- src/Components/products.jsx
+++ src/Components/products.jsx
@@ -1,62 +1,22 @@
-import { useState } from "react";
-import { useAddToCartMutation } from "../features/cart/cartApiSlice";
+import { useNavigate } from "react-router-dom";
 
-const Product = ({ product, LastAdded, setLastAdded }) => {
-  const [quantity, setQuantity] = useState(1);
-  const [addToCart, { isLoading }] = useAddToCartMutation();
+const Product = ({ product }) => {
+  const navigate = useNavigate();
 
-  const decrease = () => {
-    if (quantity > 1) setQuantity(quantity - 1);
+  const handleClick = () => {
+    navigate(`/product-details/${product._id}`);
   };
-  const increase = () => {
-    setQuantity(quantity + 1);
-  };
-
-  const onAddtoCart = async () => {
-    try {
-      await addToCart({
-        productId: product._id,
-        quantity: quantity,
-      }).unwrap();
-
-      setLastAdded(product._id);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const itemInCart = LastAdded === product._id ? "Item in Cart:✔️" : "";
 
   const content = (
-    <article className="product">
-      <h3>{product.name}</h3>
+    <article className="product" onClick={handleClick}>
       <img src={product.imageUrl} alt={product.name} className="product__img" />
+      <h4>{product.name}</h4>
       <p>
         {new Intl.NumberFormat("en-US", {
           style: "currency",
           currency: "USD",
         }).format(product.price)}
       </p>
-      <div className="QTY-wrapper">
-        <span className="QTY-label">Quantity:</span>
-
-        <div className="QTY-box">
-          <button onClick={decrease} className="QTY-btn">
-            −
-          </button>
-          <span className="QTY-number">{quantity}</span>
-
-          <button onClick={increase} className="QTY-btn">
-            +
-          </button>
-        </div>
-      </div>
-      <button className="button-add-to-cart" onClick={onAddtoCart}>
-        Add to Cart
-      </button>
-      <span style={{ paddingLeft: "10px" }}>
-        {isLoading ? "Loading..." : itemInCart}
-      </span>
     </article>
   );
 
