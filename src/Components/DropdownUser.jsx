eb91b3daaec405b82857f226f0ba5a3063c2ac1b
Charles <enagecharles27@gmail.com>
2026-02-14 00:38:04 +0800

    add avatar

--- src/Components/DropdownUser.jsx
+++ src/Components/DropdownUser.jsx
@@ -1,15 +1,29 @@
 import { useDispatch, useSelector } from "react-redux";
 import { logOut, selectCurrentUser } from "../features/auth/authSlice";
-import { useState } from "react";
+import { apiSlice } from "../api/apiSlice";
+import { useState, useEffect } from "react";
 import "../Styles/DropdownUser.css";
-import { useLogoutMutation } from "../features/auth/authApiSlice";
+import {
+  useGetUserQuery,
+  useLogoutMutation,
+} from "../features/auth/authApiSlice";
 import { useNavigate } from "react-router-dom";
 const DropdownUser = () => {
   const User = useSelector(selectCurrentUser);
+  const { data } = useGetUserQuery(User, {
+    skip: !User,
+    refetchOnMountOrArgChange: true,
+  });
   const [open, setOpen] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [logout] = useLogoutMutation();
+
+  const [avatar, setAvatar] = useState(data?.data?.image || null);
+
+  useEffect(() => {
+    setAvatar(data?.data?.image || null);
+  }, [data?.data?.image]);
 
   const handleLogout = async () => {
     try {
@@ -18,6 +32,7 @@ const DropdownUser = () => {
       console.error("Logout failed:", err);
     } finally {
       dispatch(logOut());
+      dispatch(apiSlice.util.resetApiState());
       navigate("/login");
     }
   };
@@ -29,6 +44,14 @@ const DropdownUser = () => {
           onClick={() => setOpen(!open)}
         >
           {User}
+          <img
+            className="user-icon"
+            src={
+              avatar !== null
+                ? avatar
+                : "https://png.pngtree.com/png-vector/20221130/ourmid/pngtree-user-profile-button-for-web-and-mobile-design-vector-png-image_41767880.jpg"
+            }
+          />
         </button>
         <div className={`dropdownUser-menu`}>
           <button onClick={() => navigate("/profile")}>Profile</button>
