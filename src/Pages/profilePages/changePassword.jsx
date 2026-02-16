194748858801eac3207ede1dfc7a966c83ccd5dd
Charles <enagecharles27@gmail.com>
2026-02-17 00:38:23 +0800

    change password page

---
+++ src/Pages/profilePages/changePassword.jsx
@@ -0,0 +1,125 @@
+import ProfileLayout from "../../Components/ProfileLayout";
+import { useRef, useState } from "react";
+import "../../Styles/changePassword.css";
+
+const ChangePassword = () => {
+  const [cPwd, setCPwd] = useState(false);
+  const [currentPwd, setCurrentPwd] = useState("");
+  const [newPwd, setNewPwd] = useState("");
+  const [confirmPwd, setConfirmPwd] = useState("");
+
+  const [ShowCurrentPwd, setShowCurrentPwd] = useState(false);
+  const [ShowNewPwd, setShowNewPwd] = useState(false);
+  const [ShowConfirmPwd, setShowConfirmPwd] = useState(false);
+
+  const onSubmit = () => {
+    console.log();
+  };
+  return (
+    <ProfileLayout>
+      <section className="cpwd-body">
+        <h4> ChangePassword </h4>
+        {!cPwd && (
+          <>
+            <form className="change-password-form">
+              <label htmlFor="pwd">Password: </label>
+              <input
+                className="preview-pwd"
+                type="password"
+                autoComplete="none"
+                id="pwd"
+                disabled
+                defaultValue="************"
+                readOnly
+              />
+            </form>
+            <button onClick={() => setCPwd(true)} className="cpwd-button">
+              Change Password
+            </button>
+          </>
+        )}
+        {cPwd && (
+          <>
+            <form className="change-password-form">
+              <label htmlFor="currentPwd">Current Password: </label>
+              <div className="change-pwd-input">
+                <input
+                  type={ShowCurrentPwd ? "text" : "password"}
+                  autoComplete="none"
+                  id="currentPwd"
+                  value={currentPwd}
+                  onChange={(e) => setCurrentPwd(e.target.value)}
+                />
+                <span
+                  className="toggle-password"
+                  onClick={() => setShowCurrentPwd(!ShowCurrentPwd)}
+                >
+                  {ShowCurrentPwd ? (
+                    <i className="fa-solid fa-eye"></i>
+                  ) : (
+                    <i className="fa-solid fa-eye-slash"></i>
+                  )}
+                </span>
+              </div>
+
+              <label htmlFor="newPwd">New Password: </label>
+              <div className="change-pwd-input">
+                <input
+                  type={ShowNewPwd ? "text" : "password"}
+                  autoComplete="none"
+                  id="newPwd"
+                  value={newPwd}
+                  onChange={(e) => setNewPwd(e.target.value)}
+                />
+                <span
+                  className="toggle-password"
+                  onClick={() => setShowNewPwd(!ShowNewPwd)}
+                >
+                  {ShowNewPwd ? (
+                    <i className="fa-solid fa-eye"></i>
+                  ) : (
+                    <i className="fa-solid fa-eye-slash"></i>
+                  )}
+                </span>
+              </div>
+
+              <label htmlFor="confirmPwd">Confirm Password: </label>
+              <div className="change-pwd-input">
+                <input
+                  type={ShowConfirmPwd ? "text" : "password"}
+                  autoComplete="none"
+                  id="confirmPwd"
+                  value={confirmPwd}
+                  onChange={(e) => setConfirmPwd(e.target.value)}
+                />
+                <span
+                  className="toggle-password"
+                  onClick={() => setShowConfirmPwd(!ShowConfirmPwd)}
+                >
+                  {ShowConfirmPwd ? (
+                    <i className="fa-solid fa-eye"></i>
+                  ) : (
+                    <i className="fa-solid fa-eye-slash"></i>
+                  )}
+                </span>
+              </div>
+              <div className="cancel-confirm-buttons">
+                <button
+                  onClick={() => setCPwd(false)}
+                  className="cpwd-button-cancel"
+                >
+                  Cancel
+                </button>
+                <button onClick={() => setCPwd(false)} className="cpwd-button">
+                  Confirm
+                </button>
+              </div>
+            </form>
+          </>
+        )}
+      </section>
+    </ProfileLayout>
+  );
+};
+
+export default ChangePassword;
