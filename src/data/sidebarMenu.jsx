a9493fd5743ee4d65d87d130010358bf4e163397
Charles <enagecharles27@gmail.com>
2026-02-17 00:36:57 +0800

    optimize path for Change Password

--- src/data/sidebarMenu.jsx
+++ src/data/sidebarMenu.jsx
@@ -7,7 +7,11 @@ export const profileMenu = [
       { id: "info", label: "My Profile", path: "/profile/my-profile" },
       { id: "banks", label: "Banks & Cards", path: "/profile/bank" },
       { id: "addresses", label: "Addresses", path: "/Addresses" },
-      { id: "password", label: "Change Password", path: "/change-password" },
+      {
+        id: "password",
+        label: "Change Password",
+        path: "/profile/change-password",
+      },
       { id: "privacy", label: "Privacy Settings", path: "/privacy-settings" },
     ],
   },
