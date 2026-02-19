437f2ff3aafaec3cd0c4f21e0e40aaba2fe5486b
Charles <enagecharles27@gmail.com>
2026-02-20 01:04:19 +0800

    implement spinner when loading

--- src/Pages/profilePages/changePassword.jsx
+++ src/Pages/profilePages/changePassword.jsx
@@ -9,6 +9,7 @@ import {
 import { useToast } from "../../Context/ToastContext";
 import { useNavigate } from "react-router-dom";
 import OTPModal from "../../Components/OtpModal";
+import Spinner from "../../Components/Loading";
 
 const ChangePassword = () => {
   const [cPwd, setCPwd] = useState(false);
@@ -22,9 +23,9 @@ const ChangePassword = () => {
   const [showOtpModal, setShowOtpModal] = useState(false);
 
   const { triggerToast } = useToast();
-  const [changePwd] = useChangePasswordMutation();
+  const [changePwd, { isLoading: isChangingPwd }] = useChangePasswordMutation();
   const [sendOTP] = useSendOTPMutation();
-  const [verifyOTP] = useVerifyOTPMutation();
+  const [verifyOTP, { isLoading: isVerifyingOTP }] = useVerifyOTPMutation();
   const navigate = useNavigate();
 
   const onCancel = () => {
@@ -79,113 +80,117 @@ const ChangePassword = () => {
   };
   return (
     <ProfileLayout>
-      <section className="cpwd-body">
-        <h4> ChangePassword </h4>
-        {!cPwd && (
-          <>
-            <form className="change-password-form">
-              <label htmlFor="pwd">Password: </label>
-              <input
-                className="preview-pwd"
-                type="password"
-                autoComplete="none"
-                id="pwd"
-                disabled
-                defaultValue="************"
-                readOnly
-              />
-            </form>
-            <button onClick={onChangePwd} className="cpwd-button">
-              Change Password
-            </button>
-          </>
-        )}
-        {cPwd && (
-          <>
-            <form className="change-password-form" onSubmit={onSubmit}>
-              <label htmlFor="currentPwd">Current Password: </label>
-              <div className="change-pwd-input">
+      {isChangingPwd || isVerifyingOTP ? (
+        <Spinner />
+      ) : (
+        <section className="cpwd-body">
+          <h4> ChangePassword </h4>
+          {!cPwd && (
+            <>
+              <form className="change-password-form">
+                <label htmlFor="pwd">Password: </label>
                 <input
-                  type={ShowCurrentPwd ? "text" : "password"}
+                  className="preview-pwd"
+                  type="password"
                   autoComplete="none"
-                  id="currentPwd"
-                  value={currentPwd}
-                  onChange={(e) => setCurrentPwd(e.target.value)}
+                  id="pwd"
+                  disabled
+                  defaultValue="************"
+                  readOnly
                 />
-                <span
-                  className="toggle-password"
-                  onClick={() => setShowCurrentPwd(!ShowCurrentPwd)}
-                >
-                  {ShowCurrentPwd ? (
-                    <i className="fa-solid fa-eye"></i>
-                  ) : (
-                    <i className="fa-solid fa-eye-slash"></i>
-                  )}
-                </span>
-              </div>
+              </form>
+              <button onClick={onChangePwd} className="cpwd-button">
+                Change Password
+              </button>
+            </>
+          )}
+          {cPwd && (
+            <>
+              <form className="change-password-form" onSubmit={onSubmit}>
+                <label htmlFor="currentPwd">Current Password: </label>
+                <div className="change-pwd-input">
+                  <input
+                    type={ShowCurrentPwd ? "text" : "password"}
+                    autoComplete="none"
+                    id="currentPwd"
+                    value={currentPwd}
+                    onChange={(e) => setCurrentPwd(e.target.value)}
+                  />
+                  <span
+                    className="toggle-password"
+                    onClick={() => setShowCurrentPwd(!ShowCurrentPwd)}
+                  >
+                    {ShowCurrentPwd ? (
+                      <i className="fa-solid fa-eye"></i>
+                    ) : (
+                      <i className="fa-solid fa-eye-slash"></i>
+                    )}
+                  </span>
+                </div>
 
-              <label htmlFor="newPwd">New Password: </label>
-              <div className="change-pwd-input">
-                <input
-                  type={ShowNewPwd ? "text" : "password"}
-                  autoComplete="none"
-                  id="newPwd"
-                  value={newPwd}
-                  onChange={(e) => setNewPwd(e.target.value)}
-                />
-                <span
-                  className="toggle-password"
-                  onClick={() => setShowNewPwd(!ShowNewPwd)}
-                >
-                  {ShowNewPwd ? (
-                    <i className="fa-solid fa-eye"></i>
-                  ) : (
-                    <i className="fa-solid fa-eye-slash"></i>
-                  )}
-                </span>
-              </div>
+                <label htmlFor="newPwd">New Password: </label>
+                <div className="change-pwd-input">
+                  <input
+                    type={ShowNewPwd ? "text" : "password"}
+                    autoComplete="none"
+                    id="newPwd"
+                    value={newPwd}
+                    onChange={(e) => setNewPwd(e.target.value)}
+                  />
+                  <span
+                    className="toggle-password"
+                    onClick={() => setShowNewPwd(!ShowNewPwd)}
+                  >
+                    {ShowNewPwd ? (
+                      <i className="fa-solid fa-eye"></i>
+                    ) : (
+                      <i className="fa-solid fa-eye-slash"></i>
+                    )}
+                  </span>
+                </div>
 
-              <label htmlFor="confirmPwd">Confirm Password: </label>
-              <div className="change-pwd-input">
-                <input
-                  type={ShowConfirmPwd ? "text" : "password"}
-                  autoComplete="none"
-                  id="confirmPwd"
-                  value={confirmPwd}
-                  onChange={(e) => setConfirmPwd(e.target.value)}
-                />
-                <span
-                  className="toggle-password"
-                  onClick={() => setShowConfirmPwd(!ShowConfirmPwd)}
-                >
-                  {ShowConfirmPwd ? (
-                    <i className="fa-solid fa-eye"></i>
-                  ) : (
-                    <i className="fa-solid fa-eye-slash"></i>
-                  )}
-                </span>
-              </div>
-              <div className="cancel-confirm-buttons">
-                <button
-                  type="button"
-                  onClick={onCancel}
-                  className="cpwd-button-cancel"
-                >
-                  Cancel
-                </button>
-                <button type="submit" className="cpwd-button">
-                  Confirm
-                </button>
-              </div>
-            </form>
-          </>
-        )}
-        <OTPModal
-          isOpen={showOtpModal}
-          onClose={() => setShowOtpModal(false)}
-          onVerify={handleVerifyOtp}
-        />
-      </section>
+                <label htmlFor="confirmPwd">Confirm Password: </label>
+                <div className="change-pwd-input">
+                  <input
+                    type={ShowConfirmPwd ? "text" : "password"}
+                    autoComplete="none"
+                    id="confirmPwd"
+                    value={confirmPwd}
+                    onChange={(e) => setConfirmPwd(e.target.value)}
+                  />
+                  <span
+                    className="toggle-password"
+                    onClick={() => setShowConfirmPwd(!ShowConfirmPwd)}
+                  >
+                    {ShowConfirmPwd ? (
+                      <i className="fa-solid fa-eye"></i>
+                    ) : (
+                      <i className="fa-solid fa-eye-slash"></i>
+                    )}
+                  </span>
+                </div>
+                <div className="cancel-confirm-buttons">
+                  <button
+                    type="button"
+                    onClick={onCancel}
+                    className="cpwd-button-cancel"
+                  >
+                    Cancel
+                  </button>
+                  <button type="submit" className="cpwd-button">
+                    Confirm
+                  </button>
+                </div>
+              </form>
+            </>
+          )}
+          <OTPModal
+            isOpen={showOtpModal}
+            onClose={() => setShowOtpModal(false)}
+            onVerify={handleVerifyOtp}
+          />
+        </section>
+      )}
     </ProfileLayout>
   );
 };
