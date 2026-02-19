6d4134c8c80665812516e5d46bba64af058612d8
Charles <enagecharles27@gmail.com>
2026-02-20 01:05:10 +0800

    Optimize uploading photos

--- src/Pages/profilePages/Profile.jsx
+++ src/Pages/profilePages/Profile.jsx
@@ -11,6 +11,7 @@ import ImageCropperModal from "../../Components/ImageCropperModal";
 import { selectCurrentUser } from "../../features/auth/authSlice";
 import { useSelector } from "react-redux";
 import { useToast } from "../../Context/ToastContext";
+import Spinner from "../../Components/Loading";
 
 const Profile = () => {
   const fileRef = useRef();
@@ -31,14 +32,18 @@ const Profile = () => {
     date: "",
   });
 
-  const { data, refetch } = useGetUserQuery(User, {
+  const {
+    data,
+    refetch,
+    isLoading: isGettingUser,
+  } = useGetUserQuery(User, {
     skip: !User,
     refetchOnMountOrArgChange: true,
   });
-  const [updateUser, { isLoading, isError, isSuccess }] =
+  const [updateUser, { isLoading: isUpdatingUser, isError, isSuccess }] =
     useUpdateUserMutation();
 
-  useEffect(() => {
+  const resetForm = () => {
     if (data?.data) {
       setForm({
         user: data.data.user || "",
@@ -48,6 +53,9 @@ const Profile = () => {
       });
       setPreview(data.data?.image || null);
     }
+  };
+  useEffect(() => {
+    resetForm();
   }, [data]);
 
   const handleChange = (e) => {
@@ -80,171 +88,172 @@ const Profile = () => {
     setIsEditable(false);
   };
 
-  const handleCancel = async (e) => {
-    e.preventDefault();
+  const handleCancel = async () => {
+    setCroppedImage(null);
+    resetForm();
     setIsEditable(false);
   };
 
   return (
     <ProfileLayout>
-      <section className="profile-main">
-        <section
-          className="profile-picture"
-          // src="https://png.pngtree.com/png-vector/20221130/ourmid/pngtree-user-profile-button-for-web-and-mobile-design-vector-png-image_41767880.jpg"
-        >
-          <input
-            type="file"
-            hidden
-            ref={fileRef}
-            accept="image/*"
-            onChange={(e) => {
-              const selectedFile = e.target.files[0];
-              if (!selectedFile) return;
-              if (selectedFile.size > 2 * 1024 * 1024) {
-                alert("Max 2MB");
-                return;
-              }
-
-              const imageURL = URL.createObjectURL(selectedFile);
-
-              setImageSrc(imageURL);
-              setSelectedImage(imageURL);
-            }}
-          />
-          <div
-            style={{
-              width: "150px",
-              height: "150px",
-              overflow: "hidden",
-              borderRadius: "8px",
-            }}
-          >
-            <img
-              src={
-                croppedImage
-                  ? URL.createObjectURL(croppedImage)
-                  : preview
-                    ? preview
-                    : "https://png.pngtree.com/png-vector/20221130/ourmid/pngtree-user-profile-button-for-web-and-mobile-design-vector-png-image_41767880.jpg"
-              }
-              alt="Preview"
-              onClick={() => {
-                if (imageSrc) {
-                  setSelectedImage(imageSrc);
+      {isUpdatingUser || isGettingUser ? (
+        <Spinner />
+      ) : (
+        <section className="profile-main">
+          <section className="profile-picture">
+            <input
+              type="file"
+              hidden
+              ref={fileRef}
+              accept="image/*"
+              onChange={(e) => {
+                const selectedFile = e.target.files[0];
+                if (!selectedFile) return;
+                if (selectedFile.size > 2 * 1024 * 1024) {
+                  alert("Max 2MB");
+                  return;
                 }
-              }}
-              style={{
-                width: "100%",
-                height: "100%",
-                marginTop: "10px",
-                borderRadius: "8px",
-                objectFit: "cover",
+
+                const imageURL = URL.createObjectURL(selectedFile);
+
+                setImageSrc(imageURL);
+                setSelectedImage(imageURL);
+                e.target.value = null;
               }}
             />
-          </div>
-          <button
-            className="button-upload-image"
-            type="button"
-            disabled={!isEditable}
-            onClick={() => fileRef.current.click()}
-          >
-            {!croppedImage && !preview ? "Upload Image" : "Change Image"}
-          </button>
-        </section>
-        {selectedImage && (
-          <ImageCropperModal
-            image={selectedImage}
-            aspect={1}
-            onSave={(blob) => {
-              setCroppedImage(blob);
-              setSelectedImage(null);
-            }}
-            onCancel={() => setSelectedImage(null)}
-          />
-        )}
-        <h4>My Profile</h4>
-        <p>Manage and protect your account.</p>
-        <form className="profile-form" onSubmit={handleSubmit}>
-          <div className="profile-form-div">
-            <label htmlFor="username">Username </label>
-            <section>
-              <input
-                type="text"
-                id="username"
-                autoComplete="off"
-                value={form.user}
-                onChange={handleChange}
-                disabled={!isEditable}
-              />
-            </section>
-          </div>
-          <div className="profile-form-div">
-            <label htmlFor="email">Email </label>
-            <section>
-              <input
-                type="text"
-                id="email"
-                autoComplete="off"
-                value={
-                  isEditable === false ? hideEmail(form.email) : form.email
+            <div
+              style={{
+                width: "150px",
+                height: "150px",
+                overflow: "hidden",
+                borderRadius: "8px",
+              }}
+            >
+              <img
+                src={
+                  croppedImage
+                    ? URL.createObjectURL(croppedImage)
+                    : preview
+                      ? preview
+                      : "https://png.pngtree.com/png-vector/20221130/ourmid/pngtree-user-profile-button-for-web-and-mobile-design-vector-png-image_41767880.jpg"
                 }
-                onChange={handleChange}
-                disabled={!isEditable}
+                alt="Preview"
+                onClick={() => {
+                  if (imageSrc) {
+                    setSelectedImage(imageSrc);
+                  }
+                }}
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  marginTop: "10px",
+                  borderRadius: "8px",
+                  objectFit: "cover",
+                }}
               />
-            </section>
-          </div>
-          <div className="gender-main">
-            <label htmlFor="gender">Gender</label>
-            {genders.map((g) => (
-              <div key={g} className="gender">
+            </div>
+            <button
+              className="button-upload-image"
+              type="button"
+              disabled={!isEditable}
+              onClick={() => fileRef.current.click()}
+            >
+              {!croppedImage && !preview ? "Upload Image" : "Change Image"}
+            </button>
+          </section>
+          {selectedImage && (
+            <ImageCropperModal
+              image={selectedImage}
+              aspect={1}
+              onSave={(blob) => {
+                setCroppedImage(blob);
+                setSelectedImage(null);
+              }}
+              onCancel={() => setSelectedImage(null)}
+            />
+          )}
+          <h4>My Profile</h4>
+          <p>Manage and protect your account.</p>
+          <form className="profile-form" onSubmit={handleSubmit}>
+            <div className="profile-form-div">
+              <label htmlFor="user">Username </label>
+              <section>
                 <input
-                  type="radio"
-                  id="gender"
-                  value={g}
-                  checked={form.gender === g}
+                  type="text"
+                  id="user"
+                  autoComplete="off"
+                  value={form.user}
                   onChange={handleChange}
                   disabled={!isEditable}
                 />
-                {g}
-              </div>
-            ))}
-          </div>
-          <div className="date-main">
-            <label htmlFor="date">Date of Birth</label>
-            <input
-              type="date"
-              id="date"
-              value={form.date}
-              onChange={handleChange}
-              disabled={!isEditable}
-            />
-          </div>
-          {isLoading ? (
-            <p className="isLoading">Loading Please Wait...</p>
-          ) : isEditable === false ? (
-            <button
-              className="button-Edit"
-              onClick={(e) => setIsEditable(true, e.preventDefault())}
-            >
-              <span>Edit</span>
-              <i className="fa-solid fa-pen edit-icon"></i>
-            </button>
-          ) : (
-            <div className="button-cancel-submit">
-              <button
-                className="button-Cancel"
-                type="button"
-                onClick={handleCancel}
-              >
-                Cancel
-              </button>
-              <button className="button-Submit" type="submit">
-                Submit
-              </button>
+              </section>
             </div>
-          )}
-        </form>
-      </section>
+            <div className="profile-form-div">
+              <label htmlFor="email">Email </label>
+              <section>
+                <input
+                  type="text"
+                  id="email"
+                  autoComplete="off"
+                  value={
+                    isEditable === false ? hideEmail(form.email) : form.email
+                  }
+                  onChange={handleChange}
+                  disabled={!isEditable}
+                />
+              </section>
+            </div>
+            <div className="gender-main">
+              <label htmlFor="gender">Gender</label>
+              {genders.map((g) => (
+                <div key={g} className="gender">
+                  <input
+                    type="radio"
+                    id="gender"
+                    value={g}
+                    checked={form.gender === g}
+                    onChange={handleChange}
+                    disabled={!isEditable}
+                  />
+                  {g}
+                </div>
+              ))}
+            </div>
+            <div className="date-main">
+              <label htmlFor="date">Date of Birth</label>
+              <input
+                type="date"
+                id="date"
+                value={form.date}
+                onChange={handleChange}
+                disabled={!isEditable}
+              />
+            </div>
+            {isEditable === false ? (
+              <button
+                className="button-Edit"
+                onClick={() => setIsEditable(true)}
+              >
+                <span>Edit</span>
+                <i className="fa-solid fa-pen edit-icon"></i>
+              </button>
+            ) : (
+              <div className="button-cancel-submit">
+                <button
+                  className="button-Cancel"
+                  type="button"
+                  onClick={handleCancel}
+                >
+                  Cancel
+                </button>
+                <button className="button-Submit" type="submit">
+                  Submit
+                </button>
+              </div>
+            )}
+          </form>
+        </section>
+      )}
     </ProfileLayout>
   );
 };
