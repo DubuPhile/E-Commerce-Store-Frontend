a2da245cc609e269e668164f8b3d2df2469e64f7
Charles <enagecharles27@gmail.com>
2026-02-21 00:32:28 +0800

    implement hasLocalPass

--- src/features/auth/authSlice.jsx
+++ src/features/auth/authSlice.jsx
@@ -2,18 +2,25 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const authSlice = createSlice({
   name: "auth",
-  initialState: { user: null, token: null, roles: null || [] },
+  initialState: {
+    user: null,
+    token: null,
+    roles: null || [],
+    hasLocalPassword: false,
+  },
   reducers: {
     setCredentials: (state, action) => {
-      const { user, accessToken, roles } = action.payload;
+      const { user, accessToken, roles, hasLocalPassword } = action.payload;
       state.user = user;
       state.token = accessToken;
       state.roles = roles || [];
+      state.hasLocalPassword = hasLocalPassword;
     },
     logOut: (state, action) => {
       state.user = null;
       state.token = null;
       state.roles = [];
+      state.hasLocalPassword = false;
     },
   },
 });
@@ -23,3 +30,4 @@ export default authSlice.reducer;
 export const selectCurrentUser = (state) => state.auth.user;
 export const selectCurrentToken = (state) => state.auth.token;
 export const selectCurrentRoles = (state) => state.auth.roles;
+export const hasLocalPassword = (state) => state.auth.hasLocalPassword;
