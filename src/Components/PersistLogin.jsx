4a69b77abe56f15c4ac6c919443c0fe2e71daa47
Charles <enagecharles27@gmail.com>
2026-02-24 00:04:37 +0800

    update Spinner import

--- src/Components/PersistLogin.jsx
+++ src/Components/PersistLogin.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import { useState, useEffect } from "react";
 import useRefreshToken from "../hooks/useRefreshToken";
 import useAuth from "../hooks/useAuth";
-import Spinner from "./Loading";
+import Spinner from "./Spinner";
 
 const PersistLogin = () => {
   const [isLoading, setIsLoading] = useState(true);
