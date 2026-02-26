501e828a8e58df545ded0e7ded02771a773bd463
Charles <enagecharles27@gmail.com>
2026-02-27 00:57:46 +0800

    order page

--- src/Pages/profilePages/MyOrders.jsx
+++ src/Pages/profilePages/MyOrders.jsx
@@ -1,7 +1,40 @@
 import ProfileLayout from "../../Components/ProfileLayout";
+import { useState } from "react";
+import "../../Styles/MyOrders.css";
+import processing from "../../Components/MyOrders/processing";
+import ToShip from "../../Components/MyOrders/ToShip";
+
+const tabs = [
+  { id: "processing", label: "Processing", component: processing },
+  { id: "ship", label: "To Ship", component: ToShip },
+  { id: "completed", label: "Completed", component: "" },
+];
 
 const MyOrders = () => {
-  return <ProfileLayout>MyOrders</ProfileLayout>;
+  const [activeTab, setActiveTab] = useState(tabs[0].id);
+
+  const ActiveComponent = tabs.find((tab) => tab.id === activeTab)?.component;
+  return (
+    <ProfileLayout>
+      <section className="order-body">
+        <nav className="tab-buttons">
+          {tabs.map((tab) => (
+            <button
+              key={tab.id}
+              className={activeTab === tab.id ? "active" : ""}
+              onClick={() => setActiveTab(tab.id)}
+            >
+              {tab.label}
+            </button>
+          ))}
+        </nav>
+
+        <section className="tab-content">
+          {ActiveComponent && <ActiveComponent />}
+        </section>
+      </section>
+    </ProfileLayout>
+  );
 };
 
 export default MyOrders;
