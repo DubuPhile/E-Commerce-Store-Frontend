c99055704e04a7da39ca933bba47b30064facf07
Charles <enagecharles27@gmail.com>
2026-02-28 00:05:35 +0800

    use Outlet in Layout

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,14 +1,17 @@
-import Header from "./Header"
-import Footer from "./Footer"
+import Header from "./Header";
+import Footer from "./Footer";
+import { Outlet } from "react-router-dom";
 
-const Layout = ({children}) => {
+const Layout = () => {
   return (
     <>
-        <Header />
-        <div className="body">{children}</div>
-        <Footer />
+      <Header />
+      <div className="body">
+        <Outlet />
+      </div>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
