c870004d07ed29daefdef3e900791c20a56b1ea9
Charles <enagecharles27@gmail.com>
2026-02-28 00:07:05 +0800

    to Complete page

---
+++ src/Components/MyOrders/toComplete.jsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+import { useGetOrderQuery } from "../../features/order/orderApiSlice";
+import empty from "../../assets/empty-box.png";
+
+const toComplete = () => {
+  const { data, isLoading } = useGetOrderQuery();
+
+  const [orders, setOrders] = useState([]);
+  useEffect(() => {
+    setOrders(
+      data?.data.filter((item) => item.tracking.status === "Delivered") || [],
+    );
+  }, [data?.data]);
+
+  return (
+    <section className="proccessing-body">
+      {orders.length === 0 ? (
+        <div className="empty-orders">
+          <img src={empty} />
+          <h6>No Completed Orders</h6>
+        </div>
+      ) : (
+        orders?.map((order) => (
+          <div className="order-style" key={order._id}>
+            <h4>{order.orderNumber}</h4>
+            <div className="order-status delivered">
+              {order.tracking.status}
+            </div>
+            {order.products.map((item) => (
+              <div className="prod-style" key={item._id}>
+                <div className="title-img-product">
+                  <img
+                    className="product-img"
+                    src={item.product.imageUrl}
+                  ></img>
+                </div>
+                <div className="product-price">
+                  <h5>{item.product.name}</h5>
+                  <div className="qty-price">
+                    <p>Quantity: {item.quantity}</p>
+                    <p>
+                      Price:{" "}
+                      {new Intl.NumberFormat("en-US", {
+                        style: "currency",
+                        currency: "USD",
+                      }).format(item.product.price)}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            ))}
+            {order.totalPrice != null && (
+              <p className="total-price">
+                Total:{" "}
+                {new Intl.NumberFormat("en-US", {
+                  style: "currency",
+                  currency: "USD",
+                }).format(order.totalPrice)}
+              </p>
+            )}
+          </div>
+        ))
+      )}
+    </section>
+  );
+};
+
+export default toComplete;
