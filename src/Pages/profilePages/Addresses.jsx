4ed1b810282ca4f27f3040c95225f389db194d0b
Charles <enagecharles27@gmail.com>
2026-03-10 00:58:03 +0800

    edit address modal implementation

--- src/Pages/profilePages/Addresses.jsx
+++ src/Pages/profilePages/Addresses.jsx
@@ -9,17 +9,20 @@ import { useToast } from "../../Context/ToastContext";
 import useAuth from "../../hooks/useAuth";
 import Spinner from "../../Components/Spinner";
 import AddressLine from "../../Components/AddressLine";
+import EditAddressModal from "../../Components/EditAddressModal";
 
 const Addresses = () => {
   const { auth } = useAuth();
   const [openModal, setOpenModal] = useState(false);
+  const [editAddress, setEditAddress] = useState(null);
   const [addAddress] = useAddAddressMutation();
-  const { data: getAddress, isLoading: getAddressLoad } = useGetAddressesQuery(
-    auth.token,
-    {
-      skip: !auth.token,
-    },
-  );
+  const {
+    data: getAddress,
+    isLoading: getAddressLoad,
+    refetch,
+  } = useGetAddressesQuery(auth.token, {
+    skip: !auth.token,
+  });
 
   const { triggerToast } = useToast();
   const addresses = getAddress?.data ?? [];
@@ -28,10 +31,15 @@ const Addresses = () => {
     setOpenModal(true);
   };
 
+  const handleEdit = (address) => {
+    setEditAddress(address);
+  };
+
   const onSubmitAddress = async (value) => {
     try {
       await addAddress({ value }).unwrap();
       setOpenModal(false);
+      refetch();
       triggerToast("New Address has been Added!", "success");
     } catch (err) {
       console.log(err);
@@ -41,25 +49,36 @@ const Addresses = () => {
   return (
     <section className="address-main">
       <h4> Addresses </h4>
-      <button onClick={onAddAddress}>
+      <button onClick={onAddAddress} className="add-address-button">
         Add Address <i className="fa-solid fa-plus"></i>
       </button>
-      <ul>
-        {getAddressLoad ? (
-          <Spinner />
-        ) : addresses.length === 0 ? (
-          ""
-        ) : (
-          addresses.map((address) => {
-            return <AddressLine key={address._id} address={address} />;
-          })
-        )}
-      </ul>
+      {getAddressLoad ? (
+        <Spinner />
+      ) : addresses.length === 0 ? (
+        ""
+      ) : (
+        addresses.map((address) => {
+          return (
+            <AddressLine
+              key={address._id}
+              address={address}
+              onEdit={() => handleEdit(address)}
+            />
+          );
+        })
+      )}
       <AddAddressModal
         isOpen={openModal}
         onClose={() => setOpenModal(false)}
         onSubmit={onSubmitAddress}
       />
+      {editAddress && (
+        <EditAddressModal
+          refetch={refetch}
+          address={editAddress}
+          onClose={() => setEditAddress(null)}
+        />
+      )}
     </section>
   );
 };
