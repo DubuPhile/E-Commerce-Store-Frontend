9e15ebaedfb235a3b89bdd62c43d03fb1366ec5a
Charles <enagecharles27@gmail.com>
2026-03-11 00:15:06 +0800

    address api

---
+++ src/features/address/addressApiSlice.jsx
@@ -0,0 +1,41 @@
+import { apiSlice } from "../../api/apiSlice";
+
+const addressApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    addAddress: builder.mutation({
+      query: (Data) => ({
+        url: "/user/add-address",
+        method: "POST",
+        body: Data,
+      }),
+    }),
+    getAddresses: builder.query({
+      query: () => ({
+        url: "/user/get-address",
+        method: "GET",
+        credentials: "include",
+      }),
+    }),
+    deleteAddress: builder.mutation({
+      query: (addressId) => ({
+        url: `/user/delete-address/${addressId}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Address"],
+    }),
+    updateAddress: builder.mutation({
+      query: ({ id, data }) => ({
+        url: `/user/update-address/${id}`,
+        method: "PUT",
+        body: data,
+      }),
+      invalidatesTags: ["Address"],
+    }),
+  }),
+});
+export const {
+  useAddAddressMutation,
+  useGetAddressesQuery,
+  useDeleteAddressMutation,
+  useUpdateAddressMutation,
+} = addressApiSlice;
