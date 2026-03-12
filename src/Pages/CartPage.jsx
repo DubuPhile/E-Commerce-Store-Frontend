b6f5917a040be511bd531c14c75788ad58fec32e
Charles <enagecharles27@gmail.com>
2026-03-13 00:26:11 +0800

    Optimize onSubmit order for stripe

--- src/Pages/CartPage.jsx
+++ src/Pages/CartPage.jsx
@@ -4,13 +4,18 @@ import { useGetMyCartQuery } from "../features/cart/cartApiSlice";
 import { useConfirmOrderMutation } from "../features/order/orderApiSlice";
 import "../Styles/cart.css";
 import { useToast } from "../Context/ToastContext";
+import { usePaymentIntentMutation } from "../features/payments/paymentApiSlice";
+import { PaymentModal } from "../Components/paymentModal";
 
 const CartPage = () => {
   const { triggerToast } = useToast();
+  const [clientSecret, setClientSecret] = useState(null);
+  const [orderId, setOrderId] = useState(null);
   const [confirm, setConfirm] = useState(false);
   const [cart, setCart] = useState([]);
   const { data: MyCart, isLoading } = useGetMyCartQuery();
   const [confirmOrder] = useConfirmOrderMutation();
+  const [paymentIntent] = usePaymentIntentMutation();
   const checkBoxItem = cart.filter((item) => item.checkBox === true);
 
   let pageContent = "";
@@ -37,17 +42,39 @@ const CartPage = () => {
 
   const onSubmitOrder = async () => {
     try {
-      await confirmOrder({ totalPrice, products: checkBoxItem }).unwrap();
-      triggerToast("Order Placed!", "success");
-      setConfirm(true);
+      const order = await confirmOrder({
+        totalPrice,
+        products: checkBoxItem,
+      }).unwrap();
+
+      const newOrderId = order.data._id;
+
+      setOrderId(newOrderId);
+
+      const paymentInt = await paymentIntent({ orderId: newOrderId }).unwrap();
+      setClientSecret(paymentInt.clientSecret);
     } catch (err) {
       console.log(err);
       triggerToast(`${err.data?.message || "Error to Place Order"}`, "error");
     }
   };
 
+  const handleClose = async () => {
+    try {
+      setClientSecret(null);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   pageContent = confirm ? (
     <h2>Thank you for your order.</h2>
+  ) : clientSecret ? (
+    <PaymentModal
+      setConfirm={setConfirm}
+      clientSecret={clientSecret}
+      onClose={handleClose}
+    />
   ) : (
     <>
       <h2 className="offscreen">Cart</h2>
