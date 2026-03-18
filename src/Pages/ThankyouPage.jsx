aa7669f8f1b76b79e34b6414016ff16ab8b84809
Charles <enagecharles27@gmail.com>
2026-03-19 00:21:08 +0800

    thank you page

---
+++ src/Pages/ThankyouPage.jsx
@@ -0,0 +1,16 @@
+const ThankyouPage = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "70vh",
+      }}
+    >
+      <h2>Thank you for your order.</h2>
+    </div>
+  );
+};
+
+export default ThankyouPage;
