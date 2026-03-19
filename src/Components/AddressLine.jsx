40810d94cf3382eeeca094a97807d81854b35af7
Charles <enagecharles27@gmail.com>
2026-03-20 00:09:13 +0800

    Reoptimize Addres Line

--- src/Components/AddressLine.jsx
+++ src/Components/AddressLine.jsx
@@ -1,25 +1,27 @@
 const AddressLine = ({ address, onEdit }) => {
   return (
-    <section className="address-line-main">
-      <div className="address-name-line">
-        <h5>{address.fullName}</h5>
-        <span>{address.phone}</span>
-        <button onClick={onEdit}>Edit</button>
+    <section className="select-address">
+      <div className="address-line-main">
+        <div className="address-name-line">
+          <h5>{address.fullName}</h5>
+          <span>{address.phone}</span>
+        </div>
+        <div className="address-line">
+          <p>
+            {address.street +
+              " ," +
+              address.city +
+              " ," +
+              address.province +
+              " ," +
+              address.country +
+              " ," +
+              address.postalCode}
+          </p>
+          {address.isDefault === true ? <span>Default</span> : ""}
+        </div>
       </div>
-      <div className="address-line">
-        <p>
-          {address.street +
-            " ," +
-            address.city +
-            " ," +
-            address.province +
-            " ," +
-            address.country +
-            " ," +
-            address.postalCode}
-        </p>
-        {address.isDefault === true ? <span>Default</span> : ""}
-      </div>
+      <button onClick={onEdit}>Edit</button>
     </section>
   );
 };
