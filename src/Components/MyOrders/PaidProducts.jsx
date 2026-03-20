1623d675714d3976b1501aee6adc161496232a71
Charles <enagecharles27@gmail.com>
2026-03-21 00:04:52 +0800

    show paid products

---
+++ src/Components/MyOrders/PaidProducts.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { useGetOrderQuery } from "../../features/order/orderApiSlice";
+import empty from "../../assets/empty-box.png";
+import Orders from "./Orders";
+
+const paidProducts = () => {
+  const { data, isLoading } = useGetOrderQuery();
+
+  const [orders, setOrders] = useState([]);
+  useEffect(() => {
+    setOrders(
+      data?.data.filter((item) => item.paymentStatus === ("paid" || "Paid")) ||
+        [],
+    );
+  }, [data?.data]);
+
+  return (
+    <section className="proccessing-body">
+      {orders.length === 0 ? (
+        <div className="empty-orders">
+          <img src={empty} />
+          <h6>No Proccessing Orders</h6>
+        </div>
+      ) : (
+        <Orders orders={orders} />
+      )}
+    </section>
+  );
+};
+
+export default paidProducts;
