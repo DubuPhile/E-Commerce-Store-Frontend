01388612392f066f54e857da0a2463a1634f86f6
Charles <enagecharles27@gmail.com>
2026-03-25 00:06:01 +0800

    useClickOutside implementation and open when click

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,22 +1,41 @@
-import "../Styles/Nav.css"
+import useClickOutside from "../hooks/useClickOutside";
+import "../Styles/Nav.css";
+
+import { useState, useRef } from "react";
 
 const Nav = () => {
+  const [open, setOpen] = useState(false);
+  const openRef = useRef();
+  useClickOutside(openRef, () => setOpen(false), open);
+
   return (
     <nav className="nav-bar">
-      <a href="/" className = "customer-service nav-bar-ref">Home</a>
-      <div className="nav-dropdown">
-        <a href="#" className="products nav-bar-ref">Products ▾</a>
+      <a href="/" className="customer-service nav-bar-ref">
+        Home
+      </a>
+      <div className={`nav-dropdown ${open ? "open" : ""}`}>
+        <div
+          className={`products nav-bar-ref `}
+          onClick={() => setOpen(!open)}
+          ref={openRef}
+        >
+          Products ▾
+        </div>
 
         <div className="nav-dropdown-menu">
           <a href="/phones">Phones</a>
           <a href="/laptops">Laptops</a>
-         <a href="/accessories">Accessories</a>
+          <a href="/accessories">Accessories</a>
         </div>
       </div>
-      <a href="#" className = "customer-service nav-bar-ref">Customer Service</a>
-      <a href="#" className = "contactUs nav-bar-ref">Contact Us</a>
+      <a href="#" className="customer-service nav-bar-ref">
+        Customer Service
+      </a>
+      <a href="#" className="contactUs nav-bar-ref">
+        Contact Us
+      </a>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
